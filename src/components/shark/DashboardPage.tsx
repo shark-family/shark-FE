c11d38f7971cb9193a7fa9793b30a3dcfdd34c71
parkjoohyung <dochipjh@gmail.com>
2025-10-20 13:50:59 +0900

    fix: 키바나 대시보드 url 수정

--- src/components/shark/DashboardPage.tsx
+++ src/components/shark/DashboardPage.tsx
@@ -4,12 +4,10 @@ const DashboardPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-50 p-6">
       <h1 className="text-2xl font-bold mb-6 text-blue-600">📊 Smart Aquarium Dashboard</h1>
-      <iframe
-        src="http://34.47.81.172:5601/app/dashboards#/view/0cc1156c-7c34-4a89-814c-1b3b9bbd8c60?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3A'2025-01-14T23%3A58%3A42.024Z'%2Cto%3A'2025-01-15T00%3A28%3A19.292Z'))&show-time-filter=true"
-        height="800"
-        width="1200"
-        className="border rounded-lg shadow-lg"
-      ></iframe>
+      <iframe 
+        src="http://34.64.200.25:5601/app/dashboards#/view/23d2ba98-2e99-403f-a877-df256de3c6a3?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-time-filter=true" 
+        height="800" 
+        width="1200"></iframe>
     </div>
   );
 };
